e18f09b3315c32fd966962d9b963d8eedd51c368
ShreyashParab <shreyashparab786@gmail.com>
2023-05-09 01:17:18 +0530

    counter part UI and task UI added

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ let task = []
 let input = document.getElementById('input')
 let btn = document.getElementById('btn')
 let container = document.getElementById('display')
+let checkbox = document.getElementById('taskComplete')
 let statement = ''
+
+    // getting previous stored task so that is visible even after reload
 
     let taskInLocalStorage = JSON.parse(localStorage.getItem('todo'))
     for (const i in taskInLocalStorage) {
@@ -12,6 +15,7 @@ let statement = ''
 
     btn.addEventListener('click',insertTask)
 
+    // Insert and display task
 
     function insertTask(){
         if(input.value)
@@ -32,5 +36,25 @@ let statement = ''
 
         input.value = ''
     }
+
+    // Task complete logic
+    
+
+    checkbox.addEventListener('click',taskComplete)
+
+    function taskComplete(event){
+
+        if (event.target.checked) {
+            document.getElementById('text').classList.add('taskDone')
+            document.getElementById('task').classList.add('taskDoneBg')
+        }
+        else{
+            document.getElementById('text').classList.remove('taskDone')
+            document.getElementById('task').classList.remove('taskDoneBg')
+        }
+    }
+
+    
+    
     
 
