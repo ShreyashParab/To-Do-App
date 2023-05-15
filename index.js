25c79f7a5c6d904cbe846d6ada676b7049c81a93
ShreyashParab <shreyashparab786@gmail.com>
2023-05-15 17:50:25 +0530

    logic completed

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function insertTask() {
   statement = '';
 
   for (let i = 0; i < getTask.length; i++) {
-    statement += `<div class="task" id="task">
+    statement += `<div class="task all remainingTask" id="task">
             <div class="doneTask">
                 <label>
                     <input type="checkbox" name="taskComplete" class="taskComplete">
@@ -63,7 +63,7 @@ function insertTask() {
   registerEditListeners();
 }
 
-// Register event listeners for checkboxes
+// Register event listeners for checkboxes , edit buttons and delete buttons
 function registerCheckboxListeners() {
   let checkboxes = document.getElementsByClassName('taskComplete');
 
@@ -97,9 +97,18 @@ function taskComplete(event) {
   if (checkbox.checked) {
     textElement.classList.add('taskDone');
     taskElement.classList.add('taskDoneBg');
+    taskElement.classList.add('completedTask')
+    document.getElementById('completedCount').innerText = ++completeTaskCount
+    taskElement.classList.remove('remainingTask')
+    document.getElementById('remainingCount').innerText = --remainingTaskCount;
+
   } else {
     textElement.classList.remove('taskDone');
     taskElement.classList.remove('taskDoneBg');
+    taskElement.classList.remove('completedTask')
+    document.getElementById('completedCount').innerText = --completeTaskCount
+    taskElement.classList.add('remainingTask')
+    document.getElementById('remainingCount').innerText = ++remainingTaskCount;
   }
 }
 
@@ -118,6 +127,8 @@ function taskComplete(event) {
     location.reload();
   }
   
+
+  // task edit logic
   function editTask(event) {
     btn.classList.add('hide');
     editBtn.classList.remove('hide');
@@ -132,4 +143,62 @@ function taskComplete(event) {
         location.reload();
       }
     });
+
   }
+
+  // filter counter
+  
+    let allTaskCount = document.getElementsByClassName('all').length;
+    let completeTaskCount = document.getElementsByClassName('completedTask').length;
+    let remainingTaskCount = document.getElementsByClassName('remainingTask').length;
+  
+    document.getElementById('totalCount').innerText = allTaskCount;
+    document.getElementById('remainingCount').innerText = remainingTaskCount;
+  
+
+  // filter button
+    let totalBtn = document.getElementById('totalBtn')
+    let completedBtn = document.getElementById('completedBtn')
+    let remainingBtn = document.getElementById('remainingBtn')
+
+    totalBtn.addEventListener('click',()=>{
+      let allStyle = document.getElementsByClassName('all')
+      for(let i = 0; i< allStyle.length;i++)
+      {
+        allStyle[i].style.display = 'flex'
+      } 
+    })
+    
+    completedBtn.addEventListener('click',()=>{
+      let completeStyle = document.getElementsByClassName('completedTask')
+      let remainingStyle = document.getElementsByClassName('remainingTask')
+      
+      for(let i = 0 ;i < completeStyle.length ; i++)
+      {
+        completeStyle[i].style.display = 'flex'
+      }
+
+      for(let i = 0; i<remainingStyle.length;i++)
+      {
+        remainingStyle[i].style.display = 'none'
+      }
+
+    })
+
+    remainingBtn.addEventListener('click',()=>{
+      let completeStyle = document.getElementsByClassName('completedTask')
+      let remainingStyle = document.getElementsByClassName('remainingTask')
+      
+      for(let i = 0 ;i < completeStyle.length ; i++)
+      {
+        completeStyle[i].style.display = 'none'
+      }
+
+      for(let i = 0; i<remainingStyle.length;i++)
+      {
+        remainingStyle[i].style.display = 'flex'
+      }
+    })
+  
+
+    
